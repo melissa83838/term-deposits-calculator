4617918bd68f72d6a0f03b2c560f9b0dbd43c7ec
Melissa Wu <melissa83838@gmail.com>
2025-07-13 12:25:00 +1000

    Add basic UI for the calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,103 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [initialDeposit, setInitialDeposit] = useState();
+  const [interestRate, setInterestRate] = useState();
+  const [depositPeriod, setDepositPeriod] = useState();
+  const [interestPaymentFrequency, setInterestPaymentFrequency] = useState();
+  const [finalBalance, setFinalBalance] = useState();
+
+  const calculateFinalBalance = () => {
+    console.log(
+      initialDeposit,
+      interestRate,
+      depositPeriod,
+      interestPaymentFrequency
+    );
+  };
+
+  return (
+    <>
+      <h1>Term Deposit Calculator</h1>
+      <p>
+        Use this simple calculator to calculate your final term deposit balance
+        by inputting your initial deposit amount ($), interest rate (%), deposit
+        period (month) and interest payment frequency.
+      </p>
+      <form action={calculateFinalBalance}>
+        {/* Initial deposit amount */}
+        <label>
+          Initial Deposit Amount ($):
+          <input
+            type="number"
+            value={initialDeposit}
+            onChange={(e) => {
+              setInitialDeposit(e.target.value);
+            }}
+          />
+        </label>
+        <br />
+        <br />
+
+        {/* Interest rate */}
+        <label>
+          Interest Rate (%):
+          <input
+            type="number"
+            value={interestRate}
+            onChange={(e) => {
+              setInterestRate(e.target.value);
+            }}
+          />
+        </label>
+        <br />
+        <br />
+
+        {/* Term Deposit Period */}
+        <label>
+          Term Deposit Period (month):
+          <input
+            type="number"
+            value={depositPeriod}
+            onChange={(e) => {
+              setDepositPeriod(e.target.value);
+            }}
+          />
+        </label>
+        <br />
+        <br />
+
+        {/* Interest Payment Frequency: */}
+        <label>
+          Interest Payment Frequency:
+          <select
+            value={interestPaymentFrequency}
+            onChange={(e) => {
+              setInterestPaymentFrequency(e.target.value);
+            }}
+          >
+            <option value="monthly">Monthly</option>
+            <option value="quarterly">Quarterly</option>
+            <option value="annually">Annually</option>
+            <option value="at maturity">At maturity</option>
+          </select>
+        </label>
+        <br />
+        <br />
+
+        {/* Button to calculate final balance */}
+        <input type="submit" value="Calculate" />
+
+        {/* Final Balance */}
+        {finalBalance && (
+          <>
+            <hr />
+            <p>Final Balance: ${finalBalance}</p>
+          </>
+        )}
+      </form>
+    </>
+  );
 }
 
 export default App;
